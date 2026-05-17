9707b5fd5ac48850d4710dc1d83f0d7fbe2b3b8d
Harsh7817 <harshkumar123kej@gmail.com>
2026-05-17 18:02:45 +0530

    change the file

--- app/design/billingerp/page.tsx
+++ app/design/billingerp/page.tsx
@@ -2,7 +2,7 @@ export default function FactoryDesign() {
   return (
     <div className="w-full h-screen bg-black">
       <iframe
-        src="/designs/factory/index.html"
+        src="/designs/billingerp/index.html"
         className="w-full h-full border-0"
       />
     </div>
